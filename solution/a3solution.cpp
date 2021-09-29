c427e15d2b966e4abf3b1f38d5a407857d32cf5d
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2021-09-29 05:24:14 -0400

    Finished Explicit Euler

--- solution/a3solution.cpp
+++ solution/a3solution.cpp
@@ -20,86 +20,141 @@ A3Solution::A3Solution(std::vector<Joint2D*>& joints, std::vector<Spring2D*>& sp
     m_implicit(implicit),
     m_stiffness(stiffness)
     {
-    isUptoDate = false;
-    }
+
+    this->isInitialized = false;
+}
 
 
 void A3Solution::update(Joint2D* selected, QVector2D mouse_pos){
-
-    if (selected->is_locked())
-    {
+    if (selected->is_locked()) {
         return;
     }
 
-    this->mousePos = mouse_pos;
     this->selected = selected;
+    this->mousePos = mouse_pos;
     selected->set_position(mouse_pos);
 }
 
-// This function will only get called when in FileMode = 4 which is the Demo.
 void A3Solution::update(){
 
-    if (checkIfSystemUpdated())
-    {
-        isUptoDate = false;
+    if (this->checkIfSystemUpdated()) {
+        this->isInitialized = false;
     }
 
-    if (!isUptoDate)
-    {
-        initialiseYK();
+    if (!isInitialized) {
+        this->initializeYk();
     }
 
-    qInfo()<< "SelectedIndex: " <<selected;
-
-
-    for(int i=0 ; i > this->m_moving_joints.size(); ++i)
-    {
+    for (int i=0; i<m_moving_joints.size(); i++) {
         Joint2D* joint = m_moving_joints[i];
 
+        // if the joint is the selected joint, skip and set selectedIndex = i
         if (joint == this->selected) {
             this->selectedIndex = i;
             continue;
         }
 
+        // F = Kx, look at the function to understand,
+        Vector2f springForces = calculateSpringForces(joint, joint->get_springs());
+
+        // Damping forces changes every second.
+        // However, positional damping forces were given so merely need to multiple with the velocity
+        Vector2f dampingForces = -(m_positional_damping * Vector2f(m_yk[i*4+xVelocity], m_yk[i*4+yVelocity]));
+
         // compute forces for all joints and set
-        Vector2f GravityForce = - (Vector2f::UnitY() * this->m_mass * m_gravity);
+        // don't forget gravity forces acting downwards
+        // gravity force = m*g
+        Vector2f gravityForces = - (m_mass * m_gravity * Vector2f::UnitY());
 
-        Vector2f springForce = this->calculateSpringForce(joint, joint->get_springs());
+        Vector2f totalForces = gravityForces + springForces + dampingForces;
+        // f = ma
+        // a = f/m
+        Vector2f totalAcceleration = totalForces / this->m_mass ;
 
-        // positional damping are calculated in other places.
-        Vector2f dampForce = this->calculateDampingForce(Vector2f(m_yk[i*4 + xVelocity], m_yk[i*4 + yVelocity]));
-
-        Vector2f totalForce = GravityForce + springForce + dampForce;
-        // F = ma
-        // a = F/m
-        Vector2f totalAccel = totalForce / m_mass ;
-
-        m_yk_prime[i*4 + xAccelerationPrime ] = totalAccel.x();
-        m_yk_prime[i*4 + yAccelerationPrime ] = totalAccel.y();
+        m_ykPrime[i*4 +xAccelerationPrime] = totalAcceleration.x();
+        m_ykPrime[i*4 +yAccelerationPrime] = totalAcceleration.y();
     }
 
-    this->explicitEuler(m_yk,m_yk_prime);
+    // do Explict Euler
+    explicitEuler(m_yk, m_ykPrime);
 
-    // Updating everything in UI
+    // update every positions in Ui
     for (int i=0; i<m_moving_joints.size(); ++i) {
 
-        if (i >= 0 && i == this->selectedIndex) {
-            this->selected = nullptr;
-            this->selectedIndex = -1;
+        // on the joints that selected by mouse, continue
+        if (i >= 0 && i == selectedIndex) {
+            selected = nullptr;
+            selectedIndex = -1;
             continue;
         }
 
-        // qInfo()<< "Update: " <<m_yk[i*4 + xPosition] << " " <<m_yk[i*4 + yPosition];
-        m_moving_joints[i]->set_position(QVector2D(m_yk[i*4 + xPosition], - (m_yk[i*4 + yPosition])));
+        m_moving_joints[i]->set_position(QVector2D(m_yk[i*4+xPosition],-m_yk[i*4+yPosition]));
     }
 }
 
-// Initializing the VK
-void A3Solution::initialiseYK(){
+
+bool A3Solution::checkIfSystemUpdated() {
+
+    // The jointCount will be the newest join count while m_joints is the one holding the previous joints
+    if (this->jointCount != this->m_joints.size() || this->allCurrentJoints != this->m_joints) {
+        this->allCurrentJoints.clear();
+        for (Joint2D* joint : this->m_joints) {
+            this->allCurrentJoints.push_back(joint);
+        }
+
+        this->jointCount = this->m_joints.size();
+        return true;
+    }
+
+    return false;
+}
+
+void A3Solution::explicitEuler(VectorXf& yk, VectorXf& ykPrime){
+
+    if (!m_implicit){
+        // Pretty much explicit Euler lol.
+        yk = yk + (ykPrime * this->m_timestep);
+
+        // need to update ykPrime too
+        for (int i=0; i<this->m_moving_joints.size(); ++i) {
+            if (i == selectedIndex) {
+                continue;
+            }
+
+            ykPrime[i*4+xVelocityPrime] = yk[i*4+xVelocity];
+            ykPrime[i*4+yVelocityPrime] = yk[i*4+yVelocity];
+        }
+    }
+    // For Implicit
+    else
+    {
+
+    }
+
+    // selection of mouse case
+    if (selectedIndex >= 0) {
+        int index = selectedIndex * 4;
+
+        // We only setting the position because when we are moving the joints
+        // velocity and acceleration of that spring supposed to be zero
+        yk[index + xPosition] = this->mousePos.x();
+        yk[index + yPosition] = -(this->mousePos.y());
+
+        yk[index +xVelocity] = 0.0f;
+        yk[index +yVelocity] = 0.0f;
+        ykPrime[index +xVelocityPrime] = 0.0f;
+        ykPrime[index +yVelocityPrime] = 0.0f;
+
+        ykPrime[index +xAccelerationPrime] = 0.0f;
+        ykPrime[index +yAccelerationPrime] = 0.0f;
+    }
+}
+
+// Initialize everything needed for VK and joints
+void A3Solution::initializeYk(){
     // set all moveable joints
     this->m_moving_joints.clear();
 
-    qInfo()<< "m_joints: " << m_joints.size();
     for (Joint2D* joint : this->m_joints) {
         if (!joint->is_locked()) {
             this->m_moving_joints.push_back(joint);
@@ -107,135 +162,54 @@ void A3Solution::initialiseYK(){
     }
 
     // now set yk
-    int yk_length = 4*this->m_moving_joints.size();
+    int ykSize = 4 * this->m_moving_joints.size();
+    VectorXf yk(ykSize);
+    VectorXf ykPrime(ykSize);
 
-    VectorXf yk(yk_length);
-    VectorXf yk_prime(yk_length);
+    for (int i=0; i<ykSize/4; i++) {
+        int index = i*4;
+        Joint2D* joint = m_moving_joints[i];
 
-    for (int i=0; i<yk_length; i=i+4) {
-        int jointIndex = i/4;
-        Joint2D* joint = m_moving_joints[jointIndex];
-        Vector2f jointMathPos = Vector2f(joint->get_position().x(),-(joint->get_position().y()));
+        yk[index+xPosition] = joint->get_position().x();
+        yk[index+yPosition] = -joint->get_position().y();
 
-        yk[i+xPosition] = jointMathPos.x();
-        yk[i+yPosition] = jointMathPos.y();
-
-        if (!isUptoDate) {
-            yk[i+xVelocity] = 0.0f;
-            yk[i+yVelocity] = 0.0f;
+        if (!isInitialized) {
+            yk[index+xVelocity] = 0.0f;
+            yk[index+yVelocity] = 0.0f;
         }
 
-        yk_prime[i+xVelocityPrime] = yk[i+xVelocity];
-        yk_prime[i+yVelocityPrime] = yk[i+yVelocity];
+        // 0, 1
+        ykPrime[index+xVelocityPrime] = yk[index+xVelocity];
+        ykPrime[index+yVelocityPrime] = yk[index+yVelocity];
 
-        yk_prime[i+xAccelerationPrime] = 0.0f;
-        yk_prime[i+yAccelerationPrime] = 0.0f;
+        // 2, 3
+        ykPrime[index+xAccelerationPrime] = 0.0f;
+        ykPrime[index+yAccelerationPrime] = 0.0f;
     }
 
     this->m_yk = yk;
-    this->m_yk_prime = yk_prime;
-    isUptoDate = true;
+    this->m_ykPrime = ykPrime;
+    isInitialized = true;
 }
 
-Vector2f A3Solution::calculateDampingForce(Vector2f velocity) {
-    return -1 * m_positional_damping * velocity;
-}
-
-Vector2f A3Solution::calculateSpringForce(Joint2D* joint, std::vector<Spring2D*> springs)
-{
-    std::vector<Vector2f> forces;
-    Vector2f force_spring_total = Vector2f(0.0f,0.0f);
+Vector2f A3Solution::calculateSpringForces(Joint2D* joint, std::vector<Spring2D*> springs) {
+    // initializing the variable needed.
+    Vector2f totalSpringForce = Vector2f(0.0f,0.0f);
 
     for(Spring2D* spring : springs) {
-        float currL = spring->get_length();
-        float restL = spring->get_rest_length();
-
         Joint2D* other = spring->get_other_joint(joint);
-        QVector2D temp = joint->get_position() - other->get_position();
-        Vector2f vec_to_joint = Vector2f(temp.x(), - temp.y());
-
-        // Hooke's law
+        // vector to the joint being pulled
+        Vector2f vecToJoint = Vector2f(joint->get_position().x() - other->get_position().x(),-(joint->get_position().y() - other->get_position().y()) );
+        // Hooke's Law
         // F = kx
-        Vector2f force = -1 * this->m_stiffness * (currL-restL) * vec_to_joint.normalized();
-            forces.push_back(force);
-        }
+        // X = current length - resting length
+        Vector2f force = -1 * this->m_stiffness * (spring->get_length()-spring->get_rest_length()) * vecToJoint.normalized();
 
-        for (Vector2f sForce : forces) {
-            force_spring_total.x() += sForce.x();
-            force_spring_total.y() += sForce.y();
-        }
-
-        return force_spring_total;
-}
-
-bool A3Solution::checkIfSystemUpdated(){
-
-    if (!isUptoDate) {
-        this->allCurrentJoints.clear();
-        for (Joint2D* joint : this->m_joints) {
-            this->allCurrentJoints.push_back(joint);
-        }
-        this->jointCount = this->m_joints.size();
-        return true;
+        totalSpringForce.x() += force.x();
+        totalSpringForce.y() += force.y();
     }
 
-    // if the size is different or the joints are different, remove everything and add everything again.
-    // Not optimized but yeah
-    if (jointCount != m_joints.size() || this->allCurrentJoints != this->m_joints) {
-        this->allCurrentJoints.clear();
-        for (Joint2D* joint : this->m_joints) {
-            this->allCurrentJoints.push_back(joint);
-        }
-        this->jointCount = this->m_joints.size();
-
-        return true;
-    }
-
-    return false;
-}
-
-void A3Solution::explicitEuler(VectorXf& vectorYK, VectorXf& vectorYKPrime)
-{
-    qInfo()<< "Explcit Euler " << m_moving_joints.size();
-
-    vectorYK = vectorYK + (vectorYKPrime * this->m_timestep);
-
-    // update VK prime
-    for (int i=0; i<this->m_moving_joints.size(); ++i) {
-        if (i == selectedIndex) {
-            continue;
-        }
-
-        int index = i*4 ;
-        vectorYKPrime[index + xVelocityPrime]=vectorYK[index+xVelocity];
-        vectorYKPrime[index + yVelocityPrime]=vectorYK[index+yVelocity];
-    }
-
-    // if there is selection of the mouse.
-    if (selectedIndex >= 0) {
-
-        qInfo()<< "Mouse selected joints";
-
-        // *4 because each vector has 4 elements
-        int index = selectedIndex * 4;
-        // Set Position
-
-        vectorYK[index+xPosition] = mousePos.x();
-
-        // qt and eigen math are different
-        vectorYK[index+yPosition] = - mousePos.y();
-
-        // The velocity and the acceleration have to be recalculated
-        // Setting velocity to 0 while during selection
-        vectorYK[index+xVelocity] = 0.0f;
-        vectorYK[index+yVelocity] = 0.0f;
-        vectorYKPrime[index + xVelocityPrime]= 0.0f;
-        vectorYKPrime[index + yVelocityPrime]= 0.0f;
-
-        // setting acceleration to 0 while during selection
-        vectorYKPrime[index + xAccelerationPrime] = 0.0f;
-        vectorYKPrime[index + yAccelerationPrime] = 0.0f;
-    }
+    return totalSpringForce;
 }
 
 void A3Solution::test_eigen_library(){
